4be9850f0f4bad8d850bcacc9377bec40333be28
eaptciauri <142363977+eaptciauri@users.noreply.github.com>
2023-11-25 16:33:56 -0500

    added db instace

--- rds/main.tf
+++ rds/main.tf
@@ -21,6 +21,20 @@ resource "aws_db_subnet_group" "default" {
 }
 
 
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  db_name              = "wordpress"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = "admin"
+  password             = "password"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+  db_subnet_group_name  = aws_db_subnet_group.default.name
+}
+
+
 
 
 
